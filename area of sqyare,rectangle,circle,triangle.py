de666aa272ac795364d9aef932ceaf668adc622f
thari1234-ai <thariniparthasarathy1804@gmail.com>
2024-11-28 14:24:57 +0530

    Add files via upload

---
+++ area of sqyare,rectangle,circle,triangle.py
@@ -0,0 +1,13 @@
+#area of square,rectangle,circle,triangle
+length=int(input())
+breadth=int(input())
+radius=int(input())
+side=int(input())
+area_rectangle=length*breadth
+area_square=side*side
+area_circle=3.14*radius*radius
+area_triangle=0.5*(length*breadth)
+print(area_rectangle)
+print(area_square)
+print(area_circle)
+print(area_triangle)
