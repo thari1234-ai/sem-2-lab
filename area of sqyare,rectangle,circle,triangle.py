f5e5d57db8ee5b24df0fc7a7bdd3967fb390d970
thari1234-ai <thariniparthasarathy1804@gmail.com>
2024-11-28 21:35:35 +0530

    Update area of sqyare,rectangle,circle,triangle.py

--- area of sqyare,rectangle,circle,triangle.py
+++ area of sqyare,rectangle,circle,triangle.py
@@ -3,10 +3,12 @@ length=int(input())
 breadth=int(input())
 radius=int(input())
 side=int(input())
+height=int(input())
+base=int(input())
 area_rectangle=length*breadth
 area_square=side*side
 area_circle=3.14*radius*radius
-area_triangle=0.5*(length*breadth)
+area_triangle=0.5*(height*base)
 print(area_rectangle)
 print(area_square)
 print(area_circle)
