b2fab573e965c306ae1331f9c692f4679adc2fc5
thari1234-ai <thariniparthasarathy1804@gmail.com>
2024-11-28 14:35:42 +0530

    Add files via upload

---
+++ fibonacci sem 2.py
@@ -0,0 +1,14 @@
+num=int(input())
+n1=0
+n2=1
+count=0
+if num==1:
+    print("1")
+elif num>0:
+    while num-1>count:
+        n3=n1+n2
+        n1=n2
+        n2=n3
+        count+=1
+print(n1)
+
